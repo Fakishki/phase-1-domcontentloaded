6812b146a78cbfb739a29f401de6028a065483d9
Wally Pfingsten <wallypfingsten@gmail.com>
2023-02-23 15:05:40 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const textElem = document.getElementById(`text`);
+    textElem.innerHTML = `This is really cool!`;
+})
+
+
+/* document.addEventListener("DOMContentLoaded", document.getElementByID("text") {
+  let text = "JavaScript is so cool. It lets me add text to my page programmatically.";
+  let result = text.replace("This is really cool!")
+});*/
